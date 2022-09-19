6cabfc1a6b9144aafecf7d230294f2b321589a9a
Dinesh Yerramouthu <dyerramouthu@gmail.com>
2022-09-18 22:20:49 -0500

    Added admin features

---
+++ adminPreviousRides.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "carpoolschema";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * from bookedRideDetails brd,rideDetails rd, userlogin ul where brd.rideID = rd.rideID AND brd.userID = ul.userID ";
+$result = mysqli_query($conn, $sql);
+
+$json = array();
+
+while($row = mysqli_fetch_assoc($result)){
+    $json[] = $row;
+}
+
+echo json_encode($json);
+
+mysqli_close($conn);
+?>
